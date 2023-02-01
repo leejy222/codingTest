be5ee6b938a24c1a4a2733d46072acf82d5b208d
LeeJaeYoung <leejy01022870502@gmail.com>
2023-02-01 16:28:30 +0900

    Lv.1 message clear

---
+++ src/lv0/test.java
@@ -0,0 +1,13 @@
+package lv0;
+
+public class test {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Message aa = new Message();
+		
+		System.err.println(aa.solution("d123dd"));
+	}
+
+}
