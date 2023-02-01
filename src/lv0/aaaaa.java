7c904daec65f2641a0c8996b595a7a3d3bde7df2
LeeJaeYoung <leejy01022870502@gmail.com>
2023-02-01 14:11:13 +0900

    codingTest Stu repository

---
+++ src/lv0/aaaaa.java
@@ -0,0 +1,10 @@
+package lv0;
+
+class aaaaa {
+
+	public static void main(String[] args) {
+		 System.err.println("asdasdasd");
+//		 System.err.println(args[0]);
+//		 System.err.println(args[1]);
+	}
+}
