b202e7706db31bca9fbe5179698e4557123341e6
LeeJaeYoung <leejy01022870502@gmail.com>
2023-02-01 14:24:43 +0900

    test

--- src/lv0/aaaaa.java
+++ src/lv0/aaaaa.java
@@ -5,6 +5,7 @@ class aaaaa {
 	public static void main(String[] args) {
 		 System.err.println("asdasdasd");
 //		 System.err.println(args[0]);
-//		 System.err.println(args[1]);
+//		 System.err.println(args[1]); 
+		 
 	}
 }
